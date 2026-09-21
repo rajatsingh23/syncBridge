3216db00f457408b27956344918e14dcd58ca192
Rajat Singh <rs2382001@gmail.com>
2026-09-21 21:23:54 +0530

    Security: move Django secret key to environment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR.parent / ".env")
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+rns#9nc3g1z#yf2u%lcr5n=0p-9=%b+7$%+!dv+!&c9dmxpny'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
